caababad4a647a082baadb710300169daabe05fb
stoiclionroars <stoiclionroars@gmail.com>
2022-03-22 15:50:06 +0100

    created AVLNode.

--- src/main/scala/AvlTree.scala
+++ src/main/scala/AvlTree.scala
@@ -1,2 +1,11 @@
 package dev.stoiclionroars
 
+class AVLNode[A,B](
+    var key: A,
+    var value: B,
+    var height: Int = 0,
+    var factor: Int = 0,
+    var parent: Option[AVLNode[A,B]],
+    var leftChild: Option[AVLNode[A,B]],
+    var rightChild: Option[AVLNode[A,B]]):
+  override def toString: String = s"+++\ndata(${key} -> $value), balancing=$factor,"
